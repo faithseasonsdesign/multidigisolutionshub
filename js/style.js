d82ed4a6b14f2137cd1e003254598e478a2d474b
faithseasonsdesign <67489226+faithseasonsdesign@users.noreply.github.com>
2023-07-02 21:44:39 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -14,27 +14,6 @@ function faqExpand(){
     }
 }
 
-function menuBtn(){
-    let menuBtn = document.getElementsByClassName("menu-btn");
-    let menuItems = document.getElementsByClassName("navigation-list-items-sm");
-
-    menuBtn[0].onclick = function(){
-        if(menuItems[0].style.display ="none"){
-            menuItems[0].style.display = "block";
-            menuItems[0].style.position = "absolute";
-            menuItems[0].style.backgroundColor = "black";
-            menuItems[0].style.position = "absolute";
-            menuItems[0].style.left = "0";
-            menuItems[0].style.right = "0";
-            menuItems[0].style.top = "70pt";
-            menuItems[0].style.height = "50vh";
-            menuItems[0].className = "d-flex align-items-center flex-column container-fluid py-4 justify-content-center";
-        }
-        else if(menuItems[0].className == "d-flex align-items-center flex-column container-fluid py-4 justify-content-center"){
-            menuItems[0].className = "d-none";
-        }
-    }
-}
 
 
 faqExpand();
